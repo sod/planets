817a99079b89e2f5a628c72c18a737232ca59d59
Alexander von Weiss <alexvonweiss@googlemail.com>
2015-06-05 12:39:11 +0200

    refactor

--- app/view/service/pixi/text.js
+++ app/view/service/pixi/text.js
@@ -3,8 +3,7 @@ define(['PIXI'], function(PIXI) {
 
 	text.getCentered = function() {
 		var textEntity = new PIXI.Text('');
-		textEntity.anchor.x = .5;
-		textEntity.anchor.y = .5;
+		textEntity.anchor.x = textEntity.anchor.y = .5;
 		textEntity.style.font = 'bold 15px Arial';
 		textEntity.style.fill = 0x222222;
 
