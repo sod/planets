4e46a51432196f6188ba7ee742c4f65d92a8cad9
Alexander von Weiss <alexvonweiss@googlemail.com>
2015-06-05 12:46:26 +0200

    use abstracted ticker, preload particle asset before starting animation, add particles on ParticleContainer

--- app/view/service/game.js
+++ app/view/service/game.js
@@ -2,6 +2,7 @@ requirejs.config({
 	paths: {
 		PIXI: '/pixi.js/bin/pixi',
 		TWEEN: '/tweenjs/src/Tween',
+		quadtree: '/quadtree-js/quadtree',
 		pixi: '/service/pixi',
 		service: '/service'
 	}
@@ -12,11 +13,13 @@ define([
 	'TWEEN',
 	'service/debug',
 	'pixi/entity/player',
+	'pixi/entity/particle',
 	'pixi/behaviour/controllable/byKeyboard',
 	'pixi/behaviour/camera/follow',
 	'pixi/behaviour/camera/zoom',
-	'pixi/manager/collision'
-], function(PIXI, TWEEN, debug, playerEntity, InputByKeyboard, CameraFollow, CameraZoom, CollisionManager) {
+	'pixi/manager/collision',
+	'pixi/ticker'
+], function(PIXI, TWEEN, debug, playerEntity, particleEntity, InputByKeyboard, CameraFollow, CameraZoom, CollisionManager, ticker) {
 
 	/**
 	 * @returns {HTMLElement}
@@ -45,56 +48,78 @@ define([
 		canvas.pivot.y = -(rects.height / 2);
 	}
 
-	var renderer = new PIXI.autoDetectRenderer(0, 0, {
-		backgroundColor: 0xEEEEEE,
-		antialias: true
-	});
-
-	document.getElementById('game').appendChild(renderer.view);
-
-	var canvas = new PIXI.Container();
-	var stage = new PIXI.Container();
-	canvas.addChild(stage);
-
-	// add player
-	var player = playerEntity.create();
-	player.setColor(0x0074D9);
-	player.setRadius(100);
-	player.setName('Player');
-	stage.addChild(player);
-
-	// add enemies for testing
-	Array.apply(null, Array(600)).forEach(function(number, index) {
-		var enemy = playerEntity.create();
-		enemy.setColor(0xFF4136);
-		enemy.setRadius(35);
-		enemy.x = 71 * index;
-		enemy.setName('Enemy ' + (index + 1));
-		stage.addChild(enemy);
-	});
-
-	resize(renderer, getDomContainer(), canvas);
-	window.addEventListener('resize', resize.bind(null, renderer, getDomContainer(), canvas));
-
-	debug.activate();
-
-	var ticker = PIXI.ticker.shared;
-
-	// add behaviour
-	ticker.add((new InputByKeyboard(player)).tick);
-	ticker.add((new CameraFollow(stage, player)).tick);
-	ticker.add((new CameraZoom(stage, player.acceleration)).tick);
-	ticker.add(debug.measure((new CollisionManager(stage.children)).tick));
-
-	ticker.add(function(time) {
-		stage.children.forEach(function(entity) {
-			entity.tick(time);
+	function start() {
+		var renderer = new PIXI.autoDetectRenderer(0, 0, {
+			backgroundColor: 0xEEEEEE,
+			antialias: true
 		});
-	});
 
-	ticker.add(renderer.render.bind(renderer, canvas));
+		var canvas = new PIXI.Container();
+		var stage = new PIXI.Container();
+		var players = new PIXI.Container();
+		var particles = new PIXI.Container();
 
-	window.x = getDomContainer();
-	window.renderer = renderer;
-	window.stage = stage;
+		canvas.addChild(stage);
+		stage.addChild(players);
+		stage.addChild(particles);
+
+		// add player
+		var player = playerEntity.create();
+		player.setColor(0x0074D9);
+		player.setRadius(100);
+		player.setName('Player');
+		players.addChild(player);
+
+		// add enemies for testing
+		Array.apply(null, Array(100)).forEach(function(number, index) {
+			var enemy = playerEntity.create();
+			enemy.setColor(0xFF4136);
+			enemy.setRadius(35);
+			enemy.x = 71 * index;
+			enemy.setName('Enemy ' + (index + 1));
+			players.addChild(enemy);
+		});
+
+		// add particles for testing
+		Array.apply(null, Array(1000)).forEach(function(number, index) {
+			var particle = particleEntity.create();
+			//particle.setColor(0x000000);
+			//particle.setRadius(3);
+			particle.x = 3.5 * index;
+			particles.addChild(particle);
+		});
+
+		resize(renderer, getDomContainer(), canvas);
+		window.addEventListener('resize', resize.bind(null, renderer, getDomContainer(), canvas));
+
+		window.x = getDomContainer();
+		window.renderer = renderer;
+		window.canvas = canvas;
+		window.stage = stage;
+
+		debug.setThreshold(0.5).start();
+		//debug.setThreshold(0.5).once();
+
+		// add behaviour
+		ticker.add(new InputByKeyboard(player));
+		ticker.add(new CameraFollow(stage, player));
+		ticker.add(new CameraZoom(stage, player.acceleration));
+
+		function PixiRender() {
+			renderer.render(canvas);
+		}
+
+		ticker.add(PixiRender);
+		ticker.addPerSecond(30, new CollisionManager(stage, [players.children, particles.children]));
+
+		// just render once:
+		//ticker.stop();
+		//ticker.update();
+
+		document.getElementById('game').appendChild(renderer.view);
+	}
+
+
+	PIXI.loader.add('particle', '/image/particle.png');
+	PIXI.loader.load(start);
 });
